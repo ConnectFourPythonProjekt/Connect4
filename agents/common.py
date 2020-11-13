091ae0c25519a9454318f3dcefc5ef3fc2e7e425
NikolStefanova <nikol.stefanova@campus.tu-berlin.de>
2020-11-13 13:11:51 +0100

    100% coverage + comments

--- agents/common.py
+++ agents/common.py
@@ -21,19 +21,24 @@ class SavedState:
     pass
 
 
-GenMove = Callable[
-    [np.ndarray, BoardPiece, Optional[SavedState]],  # Arguments for the generate_move function
-    Tuple[PlayerAction, Optional[SavedState]]  # Return type of the generate_move function
+GenMove = Callable[[np.ndarray, BoardPiece, Optional[SavedState]],  # Arguments for the generate_move function
+                   Tuple[PlayerAction, Optional[SavedState]]  # Return type of the generate_move function
 ]
 
 
 def initialize_game_state() -> np.ndarray:
     """
     Returns an ndarray, shape (6, 7) and data type (dtype) BoardPiece, initialized to 0 (NO_PLAYER).
+
+    Arguments:
+        ---
+
+    Return:
+        np.ndarray: initialized empty board
     """
     return np.zeros((6, 7), BoardPiece(0))
 
-# TODO:test
+
 def pretty_print_board(board: np.ndarray) -> str:
     """
     Should return `board` converted to a human readable string representation,
@@ -48,8 +53,16 @@ def pretty_print_board(board: np.ndarray) -> str:
     |  O O X X     |
     |==============|
     |0 1 2 3 4 5 6 |
+
+    Player1 == X Player 2 == O
+
+    Arguments:
+        board: ndarray representation of the board
+
+    Return:
+        str: human readable string representation of the board, human readable
     """
-    """Player1 == X Player2 == O"""
+
     EndStr = '|==============|\n|0 1 2 3 4 5 6 |'
     for row in range(6):
         tmpString = '|'
@@ -61,19 +74,24 @@ def pretty_print_board(board: np.ndarray) -> str:
             else:
                 tmpString += '  '
         tmpString += '|'
-        EndStr = tmpString + '\n' + EndStr
+        EndStr = tmpString + '\n' + EndStr  # add new string at the beginning of the last one
 
     return '|==============|\n' + EndStr
 
-# TODO: test
+
 def string_to_board(pp_board: str) -> np.ndarray:
     """
     Takes the output of pretty_print_board and turns it back into an ndarray.
     This is quite useful for debugging, when the agent crashed and you have the last
     board state as a string.
-    """
+    Player1 == X Player2 == O
 
-    """Player1 == X Player2 == O"""
+    Arguments:
+        board: human readable string representation of the board, human readable
+
+    Return:
+        ndarray: ndarray representation of the board
+    """
     BoardArr = np.zeros((6, 7), BoardPiece(0))
     splitStr = pp_board.split('\n')
     splitStr.pop(8)
@@ -89,15 +107,26 @@ def string_to_board(pp_board: str) -> np.ndarray:
                 tmpRow.append(PLAYER1)
             else:
                 tmpRow.append(NO_PLAYER)
-        BoardArr[5 - row] = np.asarray(tmpRow)
+        BoardArr[5 - row] = np.asarray(tmpRow)  # add new row at the beginning of the last one
 
     return BoardArr
 
-# TODO: copy and "else : continue"
+
+# TODO: copy test
 def apply_player_action(board: np.ndarray, action: PlayerAction, player: BoardPiece, copy: bool = False) -> np.ndarray:
     """
     Sets board[i, action] = player, where i is the lowest open row. The modified
     board is returned. If copy is True, makes a copy of the board before modifying it.
+
+    Arguments:
+        board: ndarray representation of the board
+        action: column, in which was the last action
+        player: which player was on turn
+        copy: board copy if needed
+
+    Return:
+        ndarray: ndarray representation of the board included the last action
+
     """
     if copy == True:
         board_before = board.copy()
@@ -105,75 +134,77 @@ def apply_player_action(board: np.ndarray, action: PlayerAction, player: BoardPi
         if board[i, action] == NO_PLAYER:
             board[i, action] = player
             break
-        else:
-            continue
-
     return board
 
-# TODO: last action = None
+
 def connected_four(board: np.ndarray, player: BoardPiece, last_action: Optional[PlayerAction] = None) -> bool:
     """
     Returns True if there are four adjacent pieces equal to `player` arranged
     in either a horizontal, vertical, or diagonal line. Returns False otherwise.
     If desired, the last action taken (i.e. last column played) can be provided
     for potential speed optimisation.
+
+    Arguments:
+        board: ndarray representation of the board
+        player: which player was on turn
+        last_action: column, where was the last action
+
+    Return:
+        bool: True, when current player wins, False, when game isn't won by player
     """
 
-    # with rec
+    # check if there is last action given
     if last_action is None:
-        for rec in range(7):
-            check = connected_four(board, player, rec)
+        for rec in range(7):  # set last action as column from board
+            check = connected_four(board, player, rec)  # call itself now with given last action
             if check:
-                return True
-            elif not check and rec == 6:
-                return False
-            else:
-                continue
-
-    BoardCol = (board[0:6, last_action]).T
+                return True  # there is a win
+            if not check and rec == 6:
+                return False  # there is no win on the board and we already checked all columns
 
     # find free row in this col
-    row = len(np.argwhere(board[:, last_action] == 0))
+    row = len(np.argwhere(board[:, last_action] == 0))  # counting zeros in column
     row = board.shape[0] - row - 1
 
     # check horizontal
-    BoardRow = board[row, 0:7].T
+    BoardRow = board[row, 0:7].T    # get whole row of last action
     c = 0
     for index in range(7):
         if BoardRow[index] == player:
-            c += 1
+            c += 1  # count pieces of player in a row
             if c == 4:
                 return True
         else:
             c = 0
 
     # check vertical
+    BoardCol = (board[0:6, last_action]).T  # get whole column of last action
     count = 0
     for ind in range(6):
         if BoardCol[ind] == player:
-            count += 1
+            count += 1  # count pieces of player in a row
             if count == 4:
                 return True
         else:
             count = 0
-    # test commit
+
     # check diagonal(main)
-    diagList = np.diag(board, (last_action - row))
+    diagList = np.diag(board, (last_action - row))  # get (left over right down) diagonal of last action
     counter = 0
     for value in diagList:
         if value == player:
-            counter += 1
+            counter += 1    # count pieces of player in a row
             if counter == 4:
                 return True
         else:
             counter = 0
 
     # check diagonal(opposite)
-    DiagList = np.diag(board[::-1], (last_action - (5 - row)))
+    DiagList = np.diag(board[::-1], (last_action - (5 - row)))  # get (left down right over) diagonal of last action
     C = 0
     for val in DiagList:
         if val == player:
-            C += 1
+            C += 1      # count pieces of player in a row
             if C == 4:
                 return True
         else:
@@ -186,6 +217,14 @@ def check_end_state(board: np.ndarray, player: BoardPiece, last_action: Optional
     Returns the current game state for the current `player`, i.e. has their last
     action won (GameState.IS_WIN) or drawn (GameState.IS_DRAW) the game,
     or is play still on-going (GameState.STILL_PLAYING)?
+
+     Arguments:
+        board: ndarray representation of the board
+        player: which player was on turn
+        last_action: column, where was the last action
+
+    Return:
+        GameState: current game state
     """
 
     if connected_four(board, player, last_action):
--- tests/test_common.py
+++ tests/test_common.py
@@ -10,18 +10,20 @@ def test_initialize_game_state():
     assert ret.shape == (6, 7)
     assert np.all(ret == NO_PLAYER)
 
+
 def test_apply_player_action():
     from agents.common import initialize_game_state
     from agents.common import apply_player_action
     board = initialize_game_state()
-    board[0,1] = BoardPiece(1)
-    board[0,2] = BoardPiece(2)
-    board[1,1] = BoardPiece(1)
-    ret = apply_player_action(board,2,BoardPiece(2),False)
+    board[0, 1] = BoardPiece(1)
+    board[0, 2] = BoardPiece(2)
+    board[1, 1] = BoardPiece(1)
+    ret = apply_player_action(board, 2, BoardPiece(2), False)
     assert ret.dtype == BoardPiece
     assert ret.shape == (6, 7)
-    assert isinstance(ret,np.ndarray)
-    assert ret[1,2] == BoardPiece(2)
+    assert isinstance(ret, np.ndarray)
+    assert ret[1, 2] == BoardPiece(2)
+
     board = initialize_game_state()
     ret = apply_player_action(board, 2, BoardPiece(2), False)
     assert ret[0, 2] == BoardPiece(2)
@@ -33,19 +35,20 @@ def test_apply_player_action():
     assert ret[1, 3] == BoardPiece(1)
     ret = apply_player_action(board, 2, BoardPiece(2), False)
     assert ret[2, 2] == BoardPiece(2)
-    assert isinstance(ret,np.ndarray)
+    assert isinstance(ret, np.ndarray)
     assert ret.dtype == BoardPiece
     assert ret.shape == (6, 7)
+
 
 def test_connected_four():
     from agents.common import initialize_game_state
     from agents.common import apply_player_action
     from agents.common import connected_four
     board = initialize_game_state()
-    # True tests
+
+    # TRUE TESTS
     # vertical
     apply_player_action(board, 2, BoardPiece(2), False)
-
     apply_player_action(board, 2, BoardPiece(1), False)
     apply_player_action(board, 3, BoardPiece(2), False)
     apply_player_action(board, 2, BoardPiece(1), False)
@@ -53,22 +56,23 @@ def test_connected_four():
     apply_player_action(board, 2, BoardPiece(1), False)
     apply_player_action(board, 3, BoardPiece(2), False)
     apply_player_action(board, 2, BoardPiece(1), False)
-    ret = connected_four(board,BoardPiece(1),2)
-    assert isinstance(ret,bool)
+    ret = connected_four(board, BoardPiece(1))
+    assert isinstance(ret, bool)
     assert ret == True
+
     # horizontal
     board = initialize_game_state()
     apply_player_action(board, 2, BoardPiece(1), False)
-
     apply_player_action(board, 2, BoardPiece(2), False)
     apply_player_action(board, 3, BoardPiece(1), False)
     apply_player_action(board, 2, BoardPiece(2), False)
     apply_player_action(board, 4, BoardPiece(1), False)
     apply_player_action(board, 2, BoardPiece(2), False)
     apply_player_action(board, 5, BoardPiece(1), False)
-    ret = connected_four(board,1,5)
+    ret = connected_four(board, 1, 5)
     assert isinstance(ret, bool)
     assert ret == True
+
     # left right diagonal
     board = initialize_game_state()
     apply_player_action(board, 0, BoardPiece(1), False)
@@ -86,9 +90,10 @@ def test_connected_four():
     apply_player_action(board, 4, BoardPiece(1), False)
     apply_player_action(board, 3, BoardPiece(2), False)
     apply_player_action(board, 3, BoardPiece(1), False)
-    ret = connected_four(board, 1,3)
+    ret = connected_four(board, 1, 3)
     assert isinstance(ret, bool)
     assert ret == True
+
     # right left diagonal
     board = initialize_game_state()
     apply_player_action(board, 0, BoardPiece(1), False)
@@ -111,11 +116,11 @@ def test_connected_four():
     ret = connected_four(board, 2, 0)
     assert isinstance(ret, bool)
     assert ret == True
-    #false tests
+
+    # FALSE TESTS
     # vertical
     board = initialize_game_state()
     apply_player_action(board, 2, BoardPiece(2), False)
-
     apply_player_action(board, 2, BoardPiece(1), False)
     apply_player_action(board, 3, BoardPiece(2), False)
     apply_player_action(board, 2, BoardPiece(1), False)
@@ -124,10 +129,10 @@ def test_connected_four():
     apply_player_action(board, 3, BoardPiece(2), False)
     ret = connected_four(board, BoardPiece(2), 3)
     assert ret == False
+
     # horizontal
     board = initialize_game_state()
     apply_player_action(board, 2, BoardPiece(1), False)
-
     apply_player_action(board, 2, BoardPiece(2), False)
     apply_player_action(board, 3, BoardPiece(1), False)
     apply_player_action(board, 2, BoardPiece(2), False)
@@ -136,6 +141,7 @@ def test_connected_four():
     ret = connected_four(board, 2, 2)
     assert isinstance(ret, bool)
     assert ret == False
+
     # left right diagonal
     board = initialize_game_state()
     apply_player_action(board, 0, BoardPiece(1), False)
@@ -152,8 +158,8 @@ def test_connected_four():
     apply_player_action(board, 3, BoardPiece(2), False)
     apply_player_action(board, 4, BoardPiece(1), False)
     ret = connected_four(board, BoardPiece(1), 4)
-
     assert ret == False
+
     # right left diagonal
     board = initialize_game_state()
     apply_player_action(board, 0, BoardPiece(1), False)
@@ -170,16 +176,32 @@ def test_connected_four():
     apply_player_action(board, 3, BoardPiece(2), False)
     apply_player_action(board, 4, BoardPiece(1), False)
     apply_player_action(board, 1, BoardPiece(2), False)
-
     ret = connected_four(board, 2, 1)
     assert isinstance(ret, bool)
     assert ret == False
+
+    # NO WIN TEST
+    board = initialize_game_state()
+    apply_player_action(board, 2, BoardPiece(2), False)
+    apply_player_action(board, 2, BoardPiece(1), False)
+    apply_player_action(board, 3, BoardPiece(2), False)
+    apply_player_action(board, 2, BoardPiece(1), False)
+    apply_player_action(board, 3, BoardPiece(2), False)
+    apply_player_action(board, 2, BoardPiece(1), False)
+    apply_player_action(board, 3, BoardPiece(2), False)
+    ret = connected_four(board, BoardPiece(1))
+    assert isinstance(ret, bool)
+    assert ret == False
+
+
+
 def test_check_end_state():
     from agents.common import check_end_state
     from agents.common import apply_player_action
     from agents.common import initialize_game_state
     from agents.common import GameState
     from agents.common import pretty_print_board
+
     # test 'is win'
     board = initialize_game_state()
     apply_player_action(board, 2, BoardPiece(1), False)
@@ -190,32 +212,65 @@ def test_check_end_state():
     apply_player_action(board, 2, BoardPiece(2), False)
     apply_player_action(board, 5, BoardPiece(1), False)
 
-    ret = check_end_state(board,BoardPiece(1),5)
+    ret = check_end_state(board, BoardPiece(1), 5)
     assert isinstance(ret, GameState)
     assert ret == GameState.IS_WIN
+
     # test still playing
     board = initialize_game_state()
     apply_player_action(board, 2, BoardPiece(2), False)
     apply_player_action(board, 3, BoardPiece(1), False)
-    ret = check_end_state(board,1,3)
+    ret = check_end_state(board, 1, 3)
     assert ret == GameState.STILL_PLAYING
-    #test is draw
-    board[:,0] = BoardPiece(1)
-    board[:,1:3] = BoardPiece(2)
+
+    # test is draw
+    board[:, 0] = BoardPiece(1)
+    board[:, 1:3] = BoardPiece(2)
     board[:, 3:5] = BoardPiece(1)
-    board[:,5:7] = BoardPiece(2)
-    board[3:5,:] = BoardPiece(1)
-    board[1,:] = BoardPiece(2)
-    ret = check_end_state(board,2,5)
+    board[:, 5:7] = BoardPiece(2)
+    board[3:5, :] = BoardPiece(1)
+    board[1, :] = BoardPiece(2)
+    ret = check_end_state(board, 2, 5)
     assert ret == GameState.IS_DRAW
 
 
+def test_pretty_print_board():
+    from agents.common import pretty_print_board
+    board = np.zeros((6, 7))
+    board[0, 0:7] = [1, 2, 2, 1, 1, 1, 2]
+    board[1, 0:7] = [2, 1, 2, 1, 2, 2, 1]
+    board[2, 0:7] = [1, 2, 1, 2, 0, 0, 0]
+    board[3, 0:7] = [2, 2, 1, 0, 0, 0, 0]
+    board[4, 0:7] = [1, 1, 0, 0, 0, 0, 0]
+    board[5, 0:7] = [2, 0, 0, 0, 0, 0, 0]
+    boardStr = '|==============|\n' \
+               '|O             |\n' \
+               '|X X           |\n' \
+               '|O O X         |\n' \
+               '|X O X O       |\n' \
+               '|O X O X O O X |\n' \
+               '|X O O X X X O |\n' \
+               '|==============|\n' \
+               '|0 1 2 3 4 5 6 |'
+    assert boardStr == pretty_print_board(board)
 
 
-
-
-
-
-
-
-
+def test_string_to_board():
+    from agents.common import string_to_board
+    board = np.zeros((6, 7))
+    board[0, 0:7] = [1, 2, 2, 1, 1, 1, 2]
+    board[1, 0:7] = [2, 1, 2, 1, 2, 2, 1]
+    board[2, 0:7] = [1, 2, 1, 2, 0, 0, 0]
+    board[3, 0:7] = [2, 2, 1, 0, 0, 0, 0]
+    board[4, 0:7] = [1, 1, 0, 0, 0, 0, 0]
+    board[5, 0:7] = [2, 0, 0, 0, 0, 0, 0]
+    boardStr = '|==============|\n' \
+               '|O             |\n' \
+               '|X X           |\n' \
+               '|O O X         |\n' \
+               '|X O X O       |\n' \
+               '|O X O X O O X |\n' \
+               '|X O O X X X O |\n' \
+               '|==============|\n' \
+               '|0 1 2 3 4 5 6 |'
+    assert np.all(string_to_board(boardStr) == board)
