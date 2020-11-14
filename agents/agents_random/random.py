5eadacc507706fff54cc7c63851cc67939537350
NikolStefanova <nikol.stefanova@campus.tu-berlin.de>
2020-11-14 20:56:14 +0100

    comments

--- agents/agents_random/random.py
+++ agents/agents_random/random.py
@@ -5,10 +5,19 @@ import random
 
 
 def generate_move_random(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState]) -> Tuple[PlayerAction, Optional[SavedState]]:
-    # Choose a valid, non-full column randomly and return it as `action`
-    actionList = []
+    """Choose a valid, non-full column randomly and return it as `action`
+        Arguments:
+        board: np.ndarray representing the board
+        player: whether player plays wiht X (aka Player1) or O (Player2)
+        saved_state:
+
+    Return:
+        Tuple[PlayerAction, SavedState]: returns the column, where the agent will play
+
+    """
+    actionList = []     # list with columns, where it can be played
     for col in range(board.shape[1]):
-        if np.count_nonzero(board[:, col] == 0) > 0:
+        if np.count_nonzero(board[:, col] == 0) > 0:    # check whether the column has free space
             actionList.append(col)
 
     action = random.choice(actionList)
