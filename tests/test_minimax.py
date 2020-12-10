d0335f6c1f2c016df89a2096b810b8f33401283a
NikolStefanova <nikol.stefanova@campus.tu-berlin.de>
2020-12-10 20:59:02 +0100

    tests minimax agent

--- tests/test_minimax.py
+++ tests/test_minimax.py
@@ -16,13 +16,20 @@ def test_generate_move_minimax():
     assert action == 2
 
     # block opponent
-    board = initialize_game_state()
-    apply_player_action(board, 2, BoardPiece(2), False)
-    apply_player_action(board, 2, BoardPiece(1), False)
-    apply_player_action(board, 3, BoardPiece(2), False)
-    apply_player_action(board, 2, BoardPiece(1), False)
-    apply_player_action(board, 3, BoardPiece(2), False)
-    apply_player_action(board, 2, BoardPiece(1), False)
-    apply_player_action(board, 3, BoardPiece(2), False)
-    action1, save_state = generate_move_minimax(board, BoardPiece(2), None)
-    assert action1 == 2
+    board1 = initialize_game_state()
+    apply_player_action(board1, 2, BoardPiece(2), False)
+    apply_player_action(board1, 0, BoardPiece(1), False)
+    apply_player_action(board1, 3, BoardPiece(2), False)
+    apply_player_action(board1, 2, BoardPiece(1), False)
+    apply_player_action(board1, 3, BoardPiece(2), False)
+    apply_player_action(board1, 0, BoardPiece(1), False)
+    apply_player_action(board1, 3, BoardPiece(2), False)
+    action1, save_state = generate_move_minimax(board1, BoardPiece(1), None)
+    assert action1 == 3
+
+    # board2 = initialize_game_state()
+    # apply_player_action(board2, 3, BoardPiece(2), False)
+    # apply_player_action(board2, 3, BoardPiece(1), False)
+    # apply_player_action(board2, 0, BoardPiece(2), False)
+    # action2, save_state = generate_move_minimax(board2, BoardPiece(1), None)
+    # assert action2 == 2
