baaac8a530ced68caaf5af21fb8f0071b7a26995
NikolStefanova <nikol.stefanova@campus.tu-berlin.de>
2021-02-25 18:20:23 +0100

    no more returning node without move, doc almost done, some todos left

--- agents/agents_montecarlo/monte_carlo.py
+++ agents/agents_montecarlo/monte_carlo.py
@@ -4,17 +4,15 @@ from agents.common import check_end_state, apply_player_action, GameState, Board
 import random
 import time
 
-WIN = 1  # new_leaves player wins
+WIN = 1
 DRAW = 0
 LOST = -1
-ROW = 6
-COL = 7
-PERIOD_OF_TIME = 5  # sec
+FULL = -2  # the board is full
+PERIOD_OF_TIME = 3  # in sec; set the timer
 
 
 class Node:
     def __init__(self, score=None, board_state=None, move=None, parent=None, player=None):
-        self.UCB_score = score
         self.children = []
         self.board_state = board_state
         self.move = move
@@ -22,6 +20,8 @@ class Node:
         self.simulations = 0
         self.wins = 0
         self.player = player
+        self.value = 0
+        self.explored = False  # when True node can't be selected in selection
 
     def add_node(self):
         new_node = Node()
@@ -43,6 +43,17 @@ class Tree:
 
 def generate_move_montecarlo(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState]) -> Tuple[
     PlayerAction, Optional[SavedState]]:
+    """
+       Choose a move based on Monte Carlo tree search algorithm
+
+       Arguments:
+       board: ndarray representation of the board
+       player: whether agent plays with X (Player1) or O (Player2)
+       saved_state: computation that it could reuse for future moves
+
+       Return:
+           Tuple[PlayerAction, SavedState]: returns the column, where the agent will play and agent computations
+       """
     root = Node(board_state=board, player=player)
     tree = Tree(root)
     move = MCTS(root, board, tree)
@@ -50,31 +61,42 @@ def generate_move_montecarlo(board: np.ndarray, player: BoardPiece, saved_state:
 
 
 def MCTS(root: Node, board: np.ndarray, tree: Tree) -> int:
+    """
+    Monte Carlo tree search algorithm executing for fixed amount of time
+    Arguments:
+        root: root of the tree, starting node
+        board: ndarray representation of the board
+        tree: contains all the nodes
+
+    Return:
+        int: return the best move
+    """
     tic = time.time()  # start timer
     root.board_state = board
+
     # execute algorithm until time is out
     while True:
         best_node = selection(root, tree)
         new_node, updated_tree = expansion(best_node, tree)
-        b = (new_node.parent.board_state).copy()
-        outcome = simulation(new_node, b)
-        root, final_tree = backpropagation(new_node, outcome, updated_tree)
+        outcome = simulation(new_node, new_node.parent.board_state.copy())
+        if outcome != FULL:
+            root, final_tree = backpropagation(new_node, outcome, updated_tree)
         if time.time() - tic > PERIOD_OF_TIME: break  # out of time
 
-    # find the best move
-    win_rates = [[], []]
-    for child in root.children:
-        win_rate = child.simulations
-        win_rates[0].append(win_rate)
-        win_rates[1].append(child)
-    max_score = max(win_rates[0])
-    max_score_index = win_rates[0].index(max_score)
-    best_node = win_rates[1][max_score_index]
-
-    return best_node.move
+    # find child with best move
+    win_rates = {child.move: child.value
+                 for child in root.children}
+    return max(win_rates, key=win_rates.get)
 
 
 def upper_confidence_bound(current_node: Node) -> float:
+    """
+    Upper confidence bound formula
+    Arguments:
+        current_node: node for which we will calculate UCB score
+    Returns:
+        float: UCB score
+    """
     w = current_node.wins
     s = current_node.simulations
     sp = current_node.parent.simulations
@@ -86,26 +108,36 @@ def upper_confidence_bound(current_node: Node) -> float:
 def selection(node: Node, tree: Tree) -> Node:
     """
     Returns the node that has the highest possibility of winning (UCB1)
+
+    Arguments:
+        node: root node
+        tree: contains all the nodes
+    Returns:
+        Node: return the selected node
     """
 
-    # tree with only root we select the root
+    # first select all 7 children of the root
     if (node.parent is None) and (len(node.children) < 7):
         return node
-    best_score = [[], []]
-    for node in tree.nodes:
-        node.UCB_score = upper_confidence_bound(node)
-        best_score[0].append(node.UCB_score)
-        best_score[1].append(node)
-    max_score = max(best_score[0])
-    max_score_index = best_score[0].index(max_score)
 
-    return best_score[1][max_score_index]
+    # finding best child with UCB
+    best_score = {
+        item: upper_confidence_bound(item)
+        for item in tree.nodes
+        if not item.explored
+    }
+    return max(best_score, key=best_score.get)
 
 
 def expansion(selected_node: Node, tree: Tree) -> Tuple[Node, Tree]:
     """
     The function expands the selected node and creates children node.
     Returns new node and updated tree with the newly created node
+        Arguments:
+            selected_node: selected node from selection()
+            tree: contains all the nodes
+        Returns:
+            Tuple[Node, Tree]: returns new node and updated tree
     """
 
     selected_node.add_node()
@@ -117,33 +149,39 @@ def expansion(selected_node: Node, tree: Tree) -> Tuple[Node, Tree]:
 
 def simulation(newly_created_node: Node, board: np.ndarray) -> int:
     """
-    Called until the game is finished (there is a win or a draw)
-    and return the result
+    Called until the game is finished (win, lost or draw)
+    and return the result. When there is no valid move for the node return that the board is full.
+    Arguments:
+        newly_created_node: new node
+        board: ndarray representation of the board
+    Return:
+        int: 1 for Win, -1 for Lost, 0 for Draw and  -2 for Full when the board is full
+        and the newly_created_node can't make a move
     """
 
     opponent = other_player(newly_created_node.player)
+
     move = valid_move(board, opponent, newly_created_node)
     if move is None:
-        return DRAW
+        newly_created_node.explored = True  # this node cant be explored further
+        return FULL
+
+    # set nodes value and move
     newly_created_node.move = move
-
+    board_before = board.copy()
     newly_created_node.board_state = apply_player_action(board, move, opponent, False)
+    newly_created_node.value = evaluate(newly_created_node, opponent, board_before)
+
+    # start with simulation
     board_copy = board.copy()
-
-    # direct winn
-    p, m = get_position_mask_bitmap(opponent, newly_created_node.board_state)
-    if connected_four(p):
-        newly_created_node.wins = 11000
-        return WIN
-
     on_turn = newly_created_node.player
 
     while check_end_state(board_copy, opponent, None) == GameState.STILL_PLAYING \
             and check_end_state(board_copy, newly_created_node.player, None) == GameState.STILL_PLAYING:
-        if valid_move(board_copy, on_turn, None) is None:
-            break
-        else:
-            move = valid_move(board_copy, on_turn, None)
+        move = valid_move(board_copy, on_turn, None)
+        if move is None:
+            return DRAW  # no possible moves
+
         # do moves
         if on_turn == newly_created_node.player:
             apply_player_action(board_copy, move, newly_created_node.player)
@@ -151,6 +189,7 @@ def simulation(newly_created_node: Node, board: np.ndarray) -> int:
         else:
             apply_player_action(board_copy, move, opponent)
             on_turn = newly_created_node.player
+
         # game is won
         if check_end_state(board_copy, opponent) == GameState.IS_WIN:
             return WIN
@@ -160,10 +199,57 @@ def simulation(newly_created_node: Node, board: np.ndarray) -> int:
     return DRAW
 
 
+def evaluate(node: Node, opponent: BoardPiece, board: np.ndarray) -> int:
+    """
+    Return evaluation of the board after nodes move
+    Arguments:
+        node: selected node
+        opponent: actually current player
+        board: ndarray representation of the board
+    Return
+        int: nodes value after the move
+    """
+    pos_opp, m = get_position_mask_bitmap(opponent, node.board_state)   # position after the move
+    pos_player, m = get_position_mask_bitmap(node.player, board)    # other player position before the move
+    pos_player_block, m = get_position_mask_bitmap(node.player, node.board_state)    # position after the move
+
+    if connected_four(pos_opp):
+        return 20000
+    # check after the move if we blocked the other player
+    # TODO: te pak shte sa si connected 3 ama nqma da ima prazno mqsto. s tazi prowerka ne stawa
+    elif number_of_connected(pos_player) == 3 and number_of_connected(pos_player_block) != 3:
+        return 10000
+    else:
+        return evaluate_board(pos_opp)
+
+
+def evaluate_board(position: int) -> int:
+    """
+    Evaluate board according to bit position
+    Arguments:
+         position: bit representation of board and where the player has piece
+    Return:
+        int: value of the board
+    """
+    num = number_of_connected(position)
+    if num == 3:
+        return 10000
+    elif num == 2:
+        return 100
+    else:
+        return 1
+
+
 def backpropagation(newly_created_node: Node, outcome: int, tree: Tree) -> Tuple[Node, Tree]:
     """
     Update nodes scores one by one by going up the tree until the root
     Returns the root and tree with the updated scores
+    Arguments:
+        newly_created_node: selected node
+        outcome: outcome of simulation()
+        tree: contains all the nodes
+    Return:
+        Tuple[Node, Tree]: updated node and tree values
     """
 
     newly_created_node.simulations += 1  # new node simulations
@@ -203,46 +289,29 @@ def backpropagation(newly_created_node: Node, outcome: int, tree: Tree) -> Tuple
 def valid_move(board: np.ndarray, player: BoardPiece, node: None) -> int:
     """
     Return a valid random move in the board
+    Arguments:
+        board: ndarray representation of the board
+        node: current node
     """
 
-    # opponent = other_player(player)
-    # board_copy = board.copy()
-    # next_move = [[], [], [], []]
-    # for col in range(COL):
-    #     if np.count_nonzero(board[:, col] == 0) > 0:
-    #         apply_player_action(board_copy, col, player)
-    #         position, mask = get_position_mask_bitmap(player, board_copy)
-    #         number_connected = number_of_connected(position)
-    #         next_move[number_connected - 1].append(col)
-    #         board_copy = board.copy()
-    #         apply_player_action(board_copy, col, opponent)
-    #         pos, mask = get_position_mask_bitmap(opponent, board_copy)
-    #         if connected_four(pos):
-    #             return col
-    # while True:
-    #     item = next_move.pop()
-    #     if len(item) != 0:
-    #         return random.choice(item)
-
+    # list with invalid moves
     invalid_moves = []
-    if node:
-        siblings: list = node.parent.children
-        if len(siblings) > 1:
-            for item in siblings:
-                invalid_moves.append(item.move)
+    if node and len(node.parent.children) > 1:
+        invalid_moves = [item.move for item in node.parent.children]
 
-    actionList = []  # list with columns, where it can be played
-    for col in range(board.shape[1]):
-        if np.count_nonzero(board[:, col] == 0) > 0 and col not in invalid_moves:  # check whether the column is full
-            actionList.append(col)
+    actionList = [col for col in range(7) if np.count_nonzero(board[:, col] == 0) > 0 and col not in invalid_moves]
 
-    if len(actionList) > 0:
-        return random.choice(actionList)
-    else:
-        return None
+    return random.choice(actionList) if len(actionList) > 0 else None
 
 
 def other_player(player: BoardPiece) -> BoardPiece:
+    """
+    Function returning the opponent of current player
+    Arguments:
+        player: current player
+    Return:
+        BoardPiece: opponent player
+    """
     if player == BoardPiece(1):
         return BoardPiece(2)
     else:
@@ -250,6 +319,16 @@ def other_player(player: BoardPiece) -> BoardPiece:
 
 
 def get_position_mask_bitmap(player: BoardPiece, board: np.ndarray) -> Tuple[int, int]:
+    """
+    Return bit representation of the board
+    Arguments:
+        player: player for whom we will return position
+        board: ndarray representation of the board to be converted in bitboard
+    Return:
+        Tuple[int, int]: return position and mask
+        position : bit representation of the board with players pieces
+        mask: bit representation of the board with all pieces
+    """
     position, mask = '', ''
     # Start with right-most column
     for j in range(6, -1, -1):
@@ -261,62 +340,34 @@ def get_position_mask_bitmap(player: BoardPiece, board: np.ndarray) -> Tuple[int
             mask += ['0', '1'][board[i, j] != 0]
             position += ['0', '1'][board[i, j] == player]
     return int(position, 2), int(mask, 2)
-#
-#
-# def number_of_connected(position: int) -> int:
-#     if connected_four(position):
-#         return 4
-#     elif connected_three(position):
-#         return 3
-#     elif connected_two(position):
-#         return 2
-#     else:
-#         return 1
-#
-#
-# def connected_three(position: int) -> bool:
-#     # Diagonal /
-#     m = position & (position >> 8)
-#     if m & (m >> 8):
-#         return True
-#     # Diagonal \
-#     m = position & (position >> 6)
-#     if m & (m >> 6):
-#         return True
-#
-#     # Horizontal
-#     m = position & (position >> 7)
-#     if m & (m >> 7):
-#         return True
-#
-#     # Vertical
-#     m = position & (position >> 1)
-#     if m & (m >> 1):
-#         return True
-#
-#     # Nothing found
-#     return False
-#
-#
-# def connected_two(position: int) -> bool:
-#     # Diagonal /
-#     if position & (position >> 8):
-#         return True
-#     # Diagonal \
-#     if position & (position >> 6):
-#         return True
-#
-#     # Horizontal
-#     if position & (position >> 7):
-#         return True
-#     # Vertical
-#     if position & (position >> 1):
-#         return True
-#     # Nothing found
-#     return False
+
+
+def number_of_connected(position: int) -> int:
+    """
+    Returns number of connected pieces for player by given position
+    Arguments:
+        position: bit representation of the board with players pieces
+    Return:
+        int: number of connected pieces
+    """
+    if connected_four(position):
+        return 4
+    elif connected_three(position):
+        return 3
+    elif connected_two(position):
+        return 2
+    else:
+        return 1
 
 
 def connected_four(position) -> bool:
+    """
+    Return True, when the player has connected 4
+    Arguments:
+        position: bit representation of the board with players pieces
+    Return:
+        bool: True for 4 connected
+    """
     # Horizontal check
     m = position & (position >> 7)
     if m & (m >> 14):
@@ -332,6 +383,60 @@ def connected_four(position) -> bool:
     # Vertical
     m = position & (position >> 1)
     if m & (m >> 2):
+        return True
+    # Nothing found
+    return False
+
+#TODO:
+def connected_three(position: int) -> bool:
+    """
+    Return True, when the player has connected 3
+    Arguments:
+        position: bit representation of the board with players pieces
+    Return:
+        bool: True for 3 connected
+    """
+    m = position & (position >> 8)
+    if m & (m >> 8):
+        return True
+    # Diagonal \
+    m = position & (position >> 6)
+    if m & (m >> 6):
+        return True
+    # Horizontal
+    m = position & (position >> 7)
+    if m & (m >> 7):
+        return True
+
+    # Vertical
+    m = position & (position >> 1)
+    if m & (m >> 1):
+        return True
+
+    # Nothing found
+    return False
+
+#TODO:
+def connected_two(position: int) -> bool:
+    """
+    Return True, when the player has connected 2
+    Arguments:
+        position: bit representation of the board with players pieces
+    Return:
+        bool: True for 2 connected
+    """
+    # Diagonal /
+    if position & (position >> 8):
+        return True
+    # Diagonal \
+    if position & (position >> 6):
+        return True
+
+    # Horizontal
+    if position & (position >> 7):
+        return True
+    # Vertical
+    if position & (position >> 1):
         return True
     # Nothing found
     return False
